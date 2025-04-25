c1d178e72aee61b52b6fdaee609eb76e2feaf1d8
Jack Sebben <johnjacksebben@gmail.com>
2025-04-25 17:28:32 -0400

    Made windows that open and close

--- script.js
+++ script.js
@@ -1,8 +1,67 @@
+let zIndexCounter = 1;
+
+function focusWin(win, active) {
+	// Bring window to front
+	bringFront(win);
+	
+	// Show/hide visuals to indicate focus/unfocused
+	if (active) {
+		const titleBar = win.find('.inactive-title-bar');
+		titleBar.removeClass('inactive-title-bar');
+		titleBar.addClass('title-bar');
+		titleBar.find('.resize').show();
+	} else {
+		const titleBar = win.find('.title-bar');
+		titleBar.removeClass('title-bar');
+		titleBar.addClass('inactive-title-bar');
+		titleBar.find('.resize').hide();
+	}
+}
+
+function bringFront(elem) {
+	elem.css('z-index', zIndexCounter);
+	zIndexCounter++;
+}
+
 function updateTime() {
     const now = new Date();
     const timeString = now.toLocaleTimeString("en-US", {hour: "numeric", minute: "2-digit", hour12: true});
-    document.getElementById("time").textContent = timeString;
+    $('#time').text(timeString);
+	$('#year').text(now.getFullYear());
 }
 
-setInterval(updateTime, 1000); // Update every second
-updateTime(); // Initial update
+$(document).ready(function() {
+	// Updates current time in top right
+	updateTime(); // Inital
+	setInterval(updateTime, 2000); // Update every two seconds
+	
+	
+	// If we click on desktop, make windows inactive
+	// If we click on window, make only that window active
+	$('body').on('click', function(e) {
+		const target = $(e.target);
+		const clickedWin = $(target).closest('.window');
+		
+		focusWin($('.window'), false);
+		
+		// Only do if we clicked window
+		if (clickedWin.length) {
+			focusWin(clickedWin, true);
+		}
+	});
+	
+	$('.open-win').on('click', function(e) {
+		const target = $(this).data('target');
+		const newWin = $(target);
+		newWin.show();
+		
+		// Delay focus until after the body click handler
+		setTimeout(() => {
+			focusWin(newWin, true);
+		}, 0);
+	});
+	
+	$('.window .title-bar .resize').on('click', function() {
+		$(this).closest('.window').hide();
+	});
+});
